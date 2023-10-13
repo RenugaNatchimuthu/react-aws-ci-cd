59641012ea373a4f77965cbb73ac18ecd8e4565a
Renuga Natchimuthu <renuga.10725626@ltimindtree.com>
2023-10-13 16:36:59 +0530

    new changes

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React and AWS
+          Learn React and JSX
         </a>
       </header>
     </div>
